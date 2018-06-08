a9184fdc1e27da2f84f6fa402130136a28b1f056
Kis Lázár Bence <akislb99@gmail.com>
2018-06-09 00:15:59 +0200

    Test sluggify

---
+++ phpUnitTutorial/Test/URLTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace phpUnitTutorial\Test;
+
+use phpUnitTutorial\URL;
+
+class URLTest extends \PHPUnit\Framework\TestCase
+{
+	public function testSluggifyReturnsSluggifiedString() {
+		$originalString = 'This string will be sluggified';
+		$expectedResult = 'this-string-will-be-sluggified';
+
+		$url = new URL();
+
+		$result = $url->sluggify($originalString);
+
+		$this->assertEquals($expectedResult, $result);
+	}
+
+	public function testSluggifyReturnsExpectedForStringsContainingNumbers()
+	{
+		$originalString = 'This1 string2 will3 be 44 sluggified10';
+		$expectedResult = 'this1-string2-will3-be-44-sluggified10';
+
+		$url = new URL();
+
+		$result = $url->sluggify($originalString);
+
+		$this->assertEquals($expectedResult, $result);
+	}
+
+	public function testSluggifyReturnsExpectedForStringsContainingSpecialCharacters()
+	{
+		$originalString = 'This! @string#$ %$will ()be "sluggified';
+		$expectedResult = 'this-string-will-be-sluggified';
+
+		$url = new URL();
+
+		$result = $url->sluggify($originalString);
+
+		$this->assertEquals($expectedResult, $result);
+	}
+
+	public function testSluggifyReturnsExpectedForStringsContainingNonEnglishCharacters()
+	{
+		$originalString = "Tänk efter nu – förr'n vi föser dig bort";
+		$expectedResult = 'tank-efter-nu-forrn-vi-foser-dig-bort';
+
+		$url = new URL();
+
+		$result = $url->sluggify($originalString);
+
+		$this->assertEquals($expectedResult, $result);
+	}
+
+	public function testSluggifyReturnsExpectedForEmptyStrings()
+	{
+		$originalString = '';
+		$expectedResult = '';
+
+		$url = new URL();
+
+		$result = $url->sluggify($originalString);
+
+		$this->assertEquals($expectedResult, $result);
+	}
+}
